1b99eee9540f0f17d6f4feadba541930ff1d2312
atismoas <atismoas@gmail.com>
2025-11-18 21:46:06 +0800

    carousel

--- Dairy/Day1.md
+++ Dairy/Day1.md
@@ -1,11 +1,25 @@
-这是一篇回忆录
+
 
 As Einstenid said:" Not everything that counts can be counted, and not everything that's counted truly counts."
 
 “并非所有重要的东西都是可以被计算的，也不是所有能被计算的东西都那么重要。”
 
 
-
 Wood splintered and vines tore until the enormous stalk cracked in two;
 
 木头碎裂，藤曼撕裂，巨大的茎秆断成两截；
+
+
+Everything else fades into background when your body breaks down.
+
+当身体崩溃时，其他的一切都退居幕后。
+
+
+No one is too old to grow, to learn, to begin again;
+
+并不是太老就不能成长，学习和重新开始。
+
+
+The life you want is not found in the crowd, it's waiting where few are willing to go.
+
+你想要的生活不在人群之中，它在很少人愿意去的地方等待；
--- babel-parser-demo/index.js
+++ babel-parser-demo/index.js
@@ -11,23 +11,23 @@ const ast = parser.parse(code);
 // originast = JOSN.stringify(ast)
 const originAst = { "type": "File", "start": 0, "end": 12, "loc": { "start": { "line": 1, "column": 0, "index": 0 }, "end": { "line": 1, "column": 12, "index": 12 } }, "errors": [], "program": { "type": "Program", "start": 0, "end": 12, "loc": { "start": { "line": 1, "column": 0, "index": 0 }, "end": { "line": 1, "column": 12, "index": 12 } }, "sourceType": "script", "interpreter": null, "body": [{ "type": "VariableDeclaration", "start": 0, "end": 12, "loc": { "start": { "line": 1, "column": 0, "index": 0 }, "end": { "line": 1, "column": 12, "index": 12 } }, "declarations": [{ "type": "VariableDeclarator", "start": 4, "end": 11, "loc": { "start": { "line": 1, "column": 4, "index": 4 }, "end": { "line": 1, "column": 11, "index": 11 } }, "id": { "type": "Identifier", "start": 4, "end": 5, "loc": { "start": { "line": 1, "column": 4, "index": 4 }, "end": { "line": 1, "column": 5, "index": 5 }, "identifierName": "i" }, "name": "i" }, "init": { "type": "NumericLiteral", "start": 8, "end": 11, "loc": { "start": { "line": 1, "column": 8, "index": 8 }, "end": { "line": 1, "column": 11, "index": 11 } }, "extra": { "rawValue": 888, "raw": "888" }, "value": 888 } }], "kind": "let" }], "directives": [] }, "comments": [] }
 
-console.log(ast); 
+console.log(ast);
 
 const visit = {
     // 访问哪个节点，就将哪个节点放进来
-    VariableDeclaration(path){
-        if(path.node.kind === "let"){
+    VariableDeclaration(path) {
+        if (path.node.kind === "let") {
             path.node.kind = "const";
         }
     }
 }
 
 // 游历代码，访问定义的节点
-traverse.default(ast ,visit);
-console.log(ast); 
+traverse.default(ast, visit);
+console.log(ast);
 
 //生成代码
-const res = generator.default(ast , {}, code)
+const res = generator.default(ast, {}, code)
 console.log(res.code)
 
 
